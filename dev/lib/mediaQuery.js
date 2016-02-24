f800b81783b86af13196cc6929f5eb2763d76179
Ricardo <landinezrichard@gmail.com>
2016-02-24 18:05:38 -0500

    Menu About integrado en Main Menu -mobile

--- dev/lib/mediaQuery.js
+++ dev/lib/mediaQuery.js
@@ -1,9 +1,10 @@
 var mediaQuery = (function () {
 	/* Dependencias*/
 	
-	const displayMenuAbout = require('../components/AboutMenu');
+	// const displayMenuAbout = require('../components/AboutMenu');
 	
 	const media = '(max-width: 767px)';
+	let aboutMenu = document.getElementById('aboutMenu');
 
 	/*
 	*	Añadimos un media query para que la funcionalidad touch este disponible solo en moviles.
@@ -11,11 +12,12 @@ var mediaQuery = (function () {
 
 	function media_query () {
 		
-		if( displayMenuAbout.getInstance() ){
-			displayMenuAbout.remove();
-		}
-		if(window.matchMedia(media).matches) {			
-			displayMenuAbout.init();			         
+		// if( displayMenuAbout.getInstance() ){
+		// 	displayMenuAbout.remove();
+		// }
+		if(window.matchMedia(media).matches) {
+			var hijos = aboutMenu.children;			
+			// displayMenuAbout.init();			         
 		}
 	}	
 
