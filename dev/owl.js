1c99854de5cd60bc9c0af91d31b2163bb0aba413
Ricardo <landinezrichard@gmail.com>
2016-03-01 18:01:18 -0500

    arreglado estilos layout - listo owl carousel en 'noticias.html'

--- dev/owl.js
+++ dev/owl.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
  
-  $('#owl-noticias').owlCarousel();
+  // $('#owl-noticias').owlCarousel();
+
+  $("#owl-noticias").owlCarousel({
+          itemsCustom: [
+              [0, 1]
+              
+          ],
+          navigation: false,
+          autoPlay: false,
+          slideSpeed: 1000,
+          pagination: true
+      });
  
 });
